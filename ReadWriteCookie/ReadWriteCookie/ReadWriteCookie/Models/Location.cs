e9436b097611e7c53e4a513c6385e4408b88bdde
Rod Falanga <rfalanga@users.noreply.github.com>
2025-06-24 18:23:38 -0600

    Starting to implement the dropdowns #6 #10

--- ReadWriteCookie/ReadWriteCookie/ReadWriteCookie/Models/Location.cs
+++ ReadWriteCookie/ReadWriteCookie/ReadWriteCookie/Models/Location.cs
@@ -5,6 +5,6 @@ namespace ReadWriteCookie.Models;
 
 public class Location : IInMemory
 {
-    public int ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public string PropertyName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int ID { get; set; }
+    public string PropertyName { get; set; } = string.Empty;
 }
